1b685d20904916034af9e5bf232c1504307b2904
javeria-farooq <gettingeducation6@gmail.com>
2024-01-16 12:43:56 +0500

    8th JS Assignment

---
+++ app.js
@@ -0,0 +1,31 @@
+// let num1 = 6
+// let num2 = 3
+// let res = num1++ + num1++ + ++num2 + num2++ + num1++
+//            6     +  7     +   4    +  4     +   8
+// document.write(res)
+
+// let num1 = 6
+// let num2 = 3
+// let num3 = 10
+// let res2 = --num1 - --num2 + num3++ + num3-- + num1
+// //            5   -   2    +   10   +  11    +   5
+// document.write(res2)
+
+// let a = 4
+// let b = 2
+// let c = 10
+// let e;
+// let eq = a++ + ++a - --b + c++ + e++ + c++ + ++a + a
+//           4  +  6  -  1  +  10 +     +  11 +  7  + 7 = 44
+// document.write(eq)
+
+// let y = 10
+// let u = 5
+// let w = 2
+
+// let res = y - w * u * (y + w)  + y
+        //  10 - 2 * 5 * (10 + 2) + 10
+        //  10 - 2 * 5 *     12   + 10
+        //  10 - 10    *   12     + 10
+        //  10 - 120              + 10  = -100
+// document.write(res)
